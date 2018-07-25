8d07bae93edbb885b9d2eb84850d12d14f4dabd9
Alex <chatham.alexander@gmail.com>
2018-07-25 00:12:25 -0400

    Mostly done
    need to work on create event screen, and calendar

---
+++ ConnectED/Assets/alphaAndText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class alphaAndText : MonoBehaviour {
+
+    public Image i;
+    public Text t;
+    public Color primary;
+    public Color secondary;
+
+    public void alphachange()
+    {
+        if (i.color == Color.white || i.color.a == 1f)
+        {
+            i.color = Color.clear;
+            return;
+        }
+        if (i.color == Color.clear ||i.color.a == 0)
+            i.color = Color.white;
+    }
+
+    public void textColor()
+    {
+        if (t.color == primary)
+            t.color = secondary;
+        else
+            t.color = primary;
+    }
+}
