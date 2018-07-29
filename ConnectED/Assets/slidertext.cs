2bffa0830e08350274cb8ebbfa2edd3148b7ef36
Alex <chatham.alexander@gmail.com>
2018-07-29 13:55:45 -0400

    Updated build
    Calendar page is in, Explore page is updated, filter page is more optimized for mobile, rather than desktop, with larger buttons

---
+++ ConnectED/Assets/slidertext.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class slidertext : MonoBehaviour {
+    private Text s;
+    public Slider slide;
+	// Use this for initialization
+	void Start () {
+        s = this.GetComponent<Text>();
+        s.text = slide.value.ToString() + " Miles";
+	}
+	
+    public void ChangedValue(){
+        s.text = slide.value.ToString() + " Miles";
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
