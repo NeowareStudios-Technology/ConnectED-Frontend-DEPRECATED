472cc435dfee607dfebd23d25c9d74fda05122db
Alex <chatham.alexander@gmail.com>
2018-08-03 14:34:45 -0400

    New Android Testing Build

--- ConnectED/Assets/Scripts/Jsonparser.cs
+++ ConnectED/Assets/Scripts/Jsonparser.cs
@@ -115,14 +115,14 @@ public class Jsonparser : MonoBehaviour {
             Debug.Log(profile.photo);
             //read in with texture2d.loadimage(bytedata);
         }
-        string newProfile = JsonUtility.ToJson(profile);
-        byte[] bodyRaw = Encoding.UTF8.GetBytes(newProfile);
-        UnityWebRequest www = UnityWebRequest.Post(db, newProfile);
-        www.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
-        www.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
-        www.SetRequestHeader("Content-Type", "application/json");
-        coroutine = Post(www);
-        StartCoroutine(coroutine);
+        //string newProfile = JsonUtility.ToJson(profile);
+        //byte[] bodyRaw = Encoding.UTF8.GetBytes(newProfile);
+        //UnityWebRequest www = UnityWebRequest.Post(db, newProfile);
+        //www.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        //www.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        //www.SetRequestHeader("Content-Type", "application/json");
+        //coroutine = Post(www);
+        //StartCoroutine(coroutine);
     }
 
 
@@ -131,8 +131,12 @@ public class Jsonparser : MonoBehaviour {
 
         Debug.Log("Status Code: " + www.responseCode);
     }
-	
+    public void SetProfile(Profile p)
+    {
+        profile = p;
+    }
 }
+
 
 
 [System.Serializable]
